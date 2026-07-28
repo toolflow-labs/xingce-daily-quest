4c4a007d5596dc56b4e30747d24ca64208fdad1e
toolflow-labs <hustxujinkang@gmail.com>
2026-07-28 10:02:07 +0800

    fix: load resume button runtime patch

--- papers/henan-xingce-001/visuals/visuals-07.js
+++ papers/henan-xingce-001/visuals/visuals-07.js
@@ -1,4 +1,6 @@
 /* Reserved compatibility chunk.
  * Current paper visuals are stored in visuals-01.js through visuals-06.js.
+ * Runtime compatibility fixes are loaded here until index.html is refactored.
  */
 window.XINGCE_VISUALS = window.XINGCE_VISUALS || {};
+document.write('<script src="src/runtime-fixes.js"></' + 'script>');
